eba4fd2d1a2887fef1b2a1363485666d94e5b54b
Ilan Sarveswaran <24445207+isarvesw@users.noreply.github.com>
2024-11-18 22:44:28 -0800

    respond to ping

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -12,5 +12,7 @@ object Server {
     val serverSocket = new ServerSocket()
     serverSocket.bind(new InetSocketAddress("localhost", 6379))
     val clientSocket = serverSocket.accept() // wait for client
+
+    clientSocket.getOutputStream.write("+PONG\r\n".getBytes)
   }
 }
